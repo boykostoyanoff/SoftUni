f1a05acd5f04be12aadc6c5c9e70ff8a6644ddea
boykostoyanoff <boykostoyanoff@gmail.com>
2022-06-06 06:01:06 +0300

    Create 08.Pet_Shop.py
    8.	Зоомагазин
    Напишете програма, която пресмята нужните разходи за закупуването на храна за кучета и котки.  Храната се пазарува от зоомагазин, като една опаковка храна за кучета е на цена 2.50 лв, а опаковка храна за котки струва 4 лв.
    Вход
    От конзолата се четат 2 реда:
    1.	Броят на опаковките храна за кучета – цяло число в интервала [0… 100]
    2.	Броят на опаковките храна за котки –  цяло число в интервала [0… 100]
    Изход
    На конзолата се отпечатва:
    "{крайната сума} lv."

---
+++ PythonBasics/08.Pet_Shop.py
@@ -0,0 +1,9 @@
+dog_food_price = 2.50
+cat_food_price = 4.00
+
+dog_food_count = int(input())
+cat_food_count = int(input())
+
+total_price = dog_food_price * dog_food_count + cat_food_count * cat_food_price
+
+print(f"{total_price} lv.")
