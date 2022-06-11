70f6b72555f7a6ce6cac0a163fd9842d4d7c256b
boykostoyanoff <boykostoyanoff@gmail.com>
2022-06-11 16:02:12 +0300

    Create 01. Day of Week.py

---
+++ PythonBasics/Conditional Statements Advanced - Lab/01. Day of Week.py
@@ -0,0 +1,17 @@
+day_in_number = int(input())
+if day_in_number > 7 or day_in_number < 1:
+    print('Error')
+elif day_in_number == 1:
+    print('Monday')
+elif day_in_number == 2:
+    print('Tuesday')
+elif day_in_number == 3:
+    print('Wednesday')
+elif day_in_number == 4:
+    print('Thursday')
+elif day_in_number == 5:
+    print('Friday')
+elif day_in_number == 6:
+    print('Saturday')
+elif day_in_number == 7:
+    print('Sunday')
